4d4f90dd7a2de1fba75ecbbd7ae01916d4c2e911
lsx <lsx@mac.com>
2019-04-17 21:08:57 +0800

    能显示recyclerview 内容

--- app/src/main/java/fei/li/criminalintent/CrimeFragment.java
+++ app/src/main/java/fei/li/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -13,6 +14,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import java.util.List;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
@@ -61,4 +64,5 @@ public class CrimeFragment extends Fragment {
 
         return v;
     }
+
 }
--- app/src/main/java/fei/li/criminalintent/CrimeListFragment.java
+++ app/src/main/java/fei/li/criminalintent/CrimeListFragment.java
@@ -10,16 +10,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mCrimeAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_crime_list,container,false);
+        View v = inflater.inflate(R.layout.fragment_crime_list, container, false);
         mCrimeRecyclerView = v.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        updateUI();
 
         return v;
+    }
+
+    private void updateUI() {
+        List<Crime> crimes = CrimeLab.getLab(getContext()).getCrimes();
+        mCrimeAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mCrimeAdapter);
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_crime, parent, false));
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @NonNull
+        @Override
+        public CrimeHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+            return new CrimeHolder(LayoutInflater.from(getContext()), viewGroup);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CrimeHolder crimeHolder, int i) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
     }
 }
